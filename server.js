8068f1dd80cd7d8321d91cf4fd478fe667a8826a
chestack <wangbo_bupt@163.com>
2018-08-09 11:53:45 +0800

    Customize 404 page and 500 page

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const app = express();
 
 app.use(express.static('content'));
 
-app.get('/', (req, res) => {
-  res.status(404).sendFile('index.html');
+app.all('/', (req, res) => {
+  var xMethod = req.method;
+  console.log("*********Method***************", xMethod);
+  var xCode = req.headers['x-code'];
+  console.log("*********X-Code***************", xCode);
+  if (xCode == 500){
+    res.status(500).sendFile(__dirname + '/content/500.html');
+  }else{
+    res.status(404).sendFile(__dirname + '/content/404.html');
+  }
 });
 
 app.get('/healthz', (req, res) => {
